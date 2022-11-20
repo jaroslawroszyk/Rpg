917214df09acc158b97e2d81e61c209ca2663276
JarekDev <48736575+jaroslawroszyk@users.noreply.github.com>
2022-11-20 15:48:22 +0100

    Delete build directory

--- build/external/gtest/src/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/roszyk/Desktop/rpg/Rpg/build/external/gtest/src/googletest/googletest
-# Build directory: /mnt/c/Users/roszyk/Desktop/rpg/Rpg/build/external/gtest/src/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
