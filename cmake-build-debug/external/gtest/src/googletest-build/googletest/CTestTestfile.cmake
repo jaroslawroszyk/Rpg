dc3ffbdd6bc46c0fd8bddc2216820a6ae175d435
JarekDev <48736575+jaroslawroszyk@users.noreply.github.com>
2022-11-20 15:34:55 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/external/gtest/src/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/roszyk/Desktop/rpg/Credit-Card-Validator/cmake-build-debug/external/gtest/src/googletest/googletest
-# Build directory: /mnt/c/Users/roszyk/Desktop/rpg/Credit-Card-Validator/cmake-build-debug/external/gtest/src/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
